a1218f31cc634327f060caaaf7023723de58e1cc
Her957 <stellaamponsah336@gmail.com>
2026-09-25 19:53:23 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -1,15 +1,1 @@
-const express = require("express");
-const path = require("path");
-
-const app = express();
-const PORT = process.env.PORT || 10000;
-
-app.use(express.static(path.join(__dirname, "public")));
-
-app.get("*", (_req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
-});
-
-app.listen(PORT, "0.0.0.0", () => {
-  console.log(`Heart animation running on port ${PORT}`);
-});
+const express=require("express"),path=require("path");const app=express();const PORT=process.env.PORT||10000;app.use(express.static(path.join(__dirname,"public")));app.listen(PORT,"0.0.0.0",()=>console.log(`Running on ${PORT}`));
